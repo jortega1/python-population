03305fcc9ea673eea10c934b7778c15ad8e06b93
Jose Miguel Ortega <48080043+jortega1@users.noreply.github.com>
2024-02-10 19:47:52 -0500

    Deleted test folder

--- tests/load_immigrant_data_continent.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import pandas as pd 
-conn = sqlite3.connect('stats_can_immigrant_continent.sqlite')
-
-df = pd.read_csv('immigrants_alberta_continent.csv')
-df.to_sql('immigrant_continent', conn, if_exists='replace', index=False)
-
-# print(pd.read_sql('select continent, max(population) from immigrant_continent', conn))
--- tests/read_csv.py
+++
@@ -1,5 +0,0 @@
-import pandas
-df = pandas.read_csv('population.csv', usecols=['GEO', 'VALUE'], header=0)
-#df.to_csv(header=None);
-print(df.to_csv(header=None))
-
